6275f014e7f723f286ab81a1777f1d2833a22e76
Tianmingla <1553394021@qq.com>
2026-06-17 15:16:40 +0800

    为数据库创建脚本补充use database

--- createTable.sql
+++ createTable.sql
@@ -18,7 +18,9 @@
 --
 -- Table structure for table `t_carriage`
 --
+CREATE DATABASE IF NOT EXISTS my12306 CHARACTER SET utf8mb4;
 
+USE my12306;
 DROP TABLE IF EXISTS `t_carriage`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
